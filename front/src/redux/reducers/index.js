964718a0418e77cd69e8ba78e2b8a69fcba7647a
Chorcholito <tec.diazjorge@gmail.com>
2020-11-22 23:34:00 +0100

    Update index.js

--- front/src/redux/reducers/index.js
+++ front/src/redux/reducers/index.js
@@ -15,19 +15,24 @@ const cuentasReducer = (state, action) => {
                 cuentas: state.cuentas.filter((cuentas) => cuentas.id !== action.payload),
             }
         case AGREGAR_IMPORTE_A_CUENTA:
-            //console.log(...state.cuentas.filter((cuentas) => cuentas.id===0));
+            console.log(...state.cuentas.filter((cuentas) => cuentas.id===0)); //este console.log muestra los resultados de la cuenta con id 0 (que esta hardcodeado)
             
-            console.log( state);
-            console.log( action.payload);
-
+             //console.log( state);
+             //console.log( state.cuentas);
             
- 
+            // console.log( action.payload);
                
 
             return {
-                
-                 ...state.cuentas[0],
-                 importesCargados: [...state.cuentas[0].importesCargados, action.payload]
+                ...state,
+               cuentas: [{...state.cuentas[0],//aca pienso que estaria el error. los 0 estan hardcodeados aproposito para tener control de lo que quiero hacer. 
+               importesCargados: [...state.cuentas[0].importesCargados, action.payload]//aca pienso que estaria el error. 
+            }]
+
+
+
+                //  ...state.cuentas[0],
+                //  importesCargados: [...state.cuentas[0].importesCargados, action.payload]
             }
         default:
             return state;
